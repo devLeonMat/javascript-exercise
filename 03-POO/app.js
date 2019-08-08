8223236cb1b14cecea6d13d182bb49c90269883f
leonmatias2015 <leonmatias1991@gmail.com>
2019-08-08 10:26:29 -0500

    add change

--- 03-POO/app.js
+++ 03-POO/app.js
@@ -6,11 +6,38 @@ class Tarea{
         this.nombre = nombre;
         this.prioridad = prioridad;
     }
+
+    mostrar(){
+        return `${this.nombre} tiene una prioridad ${this.prioridad}`;
+    }
 }
+
+//herencia
+class ComprarPendientes extends Tarea{
+    constructor(nombre, prioridad, cantidad){
+        super(nombre, prioridad); //hacemos referencia del constructor padre
+        this.cantidad= cantidad;
+    }
+}
+
 
 
 //creamos objetos 
 
 let tarea1 = new Tarea('Aprender Javascript', 'Alta');
+let tarea2 = new Tarea('Aprender Javascript', 'Media');
+let tarea3 = new Tarea('Aprender Javascript', 'baja');
+let tarea4 = new Tarea('Aprender Javascript', 'Media');
+let tarea5 = new Tarea('Aprender Javascript', 'Alta');
 
-console.log(tarea1);
+console.log(tarea1.mostrar());
+console.log(tarea2.mostrar());
+console.log(tarea3.mostrar());
+console.log(tarea4.mostrar());
+console.log(tarea5.mostrar());
+
+
+
+let compra1 = new ComprarPendientes('jabon', 'urgente', 3);
+
+console.log(compra1);
